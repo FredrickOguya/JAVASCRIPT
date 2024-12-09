90e4da8e4d58ccb126b01e985f205208d569ac90
FredrickOguya <onyangofredrickoguya@gmail.com>
2024-12-09 19:16:42 +0300

    update

--- revise-rock-paper-scissor.js
+++ revise-rock-paper-scissor.js
@@ -1,6 +1,12 @@
+const count = {
+        Win:0,
+        Losses:0,
+        Tie:0
+    }
 function play(move){
     let randomNumber=Math.random();
     let computerMove;
+    
 
     if(randomNumber>0 && randomNumber<1/3){
         computerMove = 'Rock';
@@ -10,10 +16,45 @@ function play(move){
         computerMove ='Scissors';
     }
     if(computerMove==='Rock'){
-        console.log(` You: ${move}\n Computer: Rock\n You Tie`)
+        if (move==='Rock'){
+            console.log(` You: ${move}\n Computer: Rock\n You Tie`)
+            result = 'Tie';
+        }else if(move === 'Paper'){
+            console.log(` You: ${move}\n Computer: Rock\n You Win`)
+            result = 'win';   
+        }else if(move === 'Scissors'){
+            console.log(` You: ${move}\n Computer: Rock\n You Lose`)
+            result = 'loss'   
+        }
     }else if(computerMove==='Paper'){
-        console.log(` You: ${move}\n Computer: Paper\n You lose`)
+        if (move==='Rock'){
+            console.log(` You: ${move}\n Computer: Paper\n You Lose`)
+            result = 'loss';
+        }else if(move === 'Paper'){
+            console.log(` You: ${move}\n Computer: Paper\n You Tie`) 
+            result = 'Tie';  
+        }else if(move === 'Scissors'){
+            console.log(` You: ${move}\n Computer: Paper\n You Win`)
+            result = 'win'   
+        }
     }else if(computerMove==='Scissors'){
-        console.log(` You: ${move}\n Computer: Scissors\n You Win`)
+        if (move==='Rock'){
+            console.log(` You: ${move}\n Computer: Scissors\n You Win`);
+            result = 'win';
+        }else if(move === 'Paper'){
+            console.log(` You: ${move}\n Computer: Scissors\n You Lose`);
+            result = 'loss';   
+        }else if(move === 'Scissors'){
+            console.log(` You: ${move}\n Computer: Scissors\n You Tie`);
+            result = 'Tie';   
+        }
     }
+    if (result==='win'){
+        count.Win+=1;
+    }else if (result==='loss'){
+        count.Losses+=1;
+    }else if(result ==='Tie'){
+        count.Tie+=1;
+    }
+    console.log(count)
 }
